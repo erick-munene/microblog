162b4dab1ee473a0c1656493b34a55cae3901e76
Erick-Munene <munenewahome@gmail.com>
2016-05-09 10:45:42 +0300

    configured the login page

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
+#=====================list of open ids====================
+
 OPENID_PROVIDERS = [
     {'name': 'Google', 'url': 'https://www.google.com/accounts/o8/id'},
     {'name': 'Yahoo', 'url': 'https://me.yahoo.com'},
     {'name': 'AOL', 'url': 'http://openid.aol.com/<username>'},
     {'name': 'Flickr', 'url': 'http://www.flickr.com/<username>'},
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}]
+
