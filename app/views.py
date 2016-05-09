cc677093b1b98e2e23782c856def9637591f25f8
Erick-Munene <munenewahome@gmail.com>
2016-05-09 10:01:24 +0300

    modified the receiving function

--- app/views.py
+++ app/views.py
@@ -21,16 +21,29 @@ def index():
                            user=user,
                            posts=posts)
 
-
+#=============this is the loging form=================
 from flask import render_template, flash, redirect
 from app import app
 from .forms import LoginForm
 
 # index view function suppressed for brevity
 
+
+#=============this is the loging form end=================
+
+
+##=============this is the receiving form data=================
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+              (form.openid.data, str(form.remember_me.data)))
+        return redirect('/index')
     return render_template('login.html', 
                            title='Sign In',
                            form=form)
+#
+##=============this is the receiving form data end=============
+
