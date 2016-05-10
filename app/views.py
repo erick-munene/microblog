9c30c08c69cc325f625599d4e0c0df7b26f2eb60
Erick-Munene <munenewahome@gmail.com>
2016-05-10 18:45:20 +0300

    updated the loging functionality

--- app/views.py
+++ app/views.py
@@ -22,10 +22,11 @@ def index():
                            posts=posts)
 
 #=============this is the loging form=================
-from flask import render_template, flash, redirect
-from app import app
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from app import app, db, lm, oid
 from .forms import LoginForm
-
+from .models import User
 # index view function suppressed for brevity
 
 
@@ -35,16 +36,25 @@ from .forms import LoginForm
 ##=============this is the receiving form data=================
 
 @app.route('/login', methods=['GET', 'POST'])
+@oid.loginhandler
 def login():
+    if g.user is not None and g.user.is_authenticated:
+        return redirect(url_for('index'))
     form = LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for OpenID="%s", remember_me=%s' %
-              (form.openid.data, str(form.remember_me.data)))
-        return redirect('/index')
+        session['remember_me'] = form.remember_me.data
+        return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
     return render_template('login.html', 
                            title='Sign In',
                            form=form,
                            providers=app.config['OPENID_PROVIDERS'])
 #
 ##=============this is the receiving form data end=============
+
+
+##=============this calls the user from database===============
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+##============================ end ============================
 
