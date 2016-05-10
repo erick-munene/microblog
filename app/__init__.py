84be91f17e6f6cac87e5a9ba9a65fc9f9ff6aa58
Erick-Munene <munenewahome@gmail.com>
2016-05-10 18:32:15 +0300

    added login configurations

--- app/__init__.py
+++ app/__init__.py
@@ -6,3 +6,14 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 
 from app import views, models
+
+#loging extention
+
+import os
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
+
+lm = LoginManager()
+lm.init_app(app)
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
